96c04776174f4b8c861534b1ca79bf3e3cded938
gdad-s-river <arihantverma1994@gmail.com>
2016-11-17 09:31:33 +0530

    (add): working copy of desktop version, some text added

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function myCallback() {
-  console.log("is it working?");
+new Vivus('b1', {
+  duration: 400,
+  type: 'oneByOne',
+  file: "exported-svgs/dear.svg"
+}, func1);
+
+
+function func1() {
+  new Vivus('b2', {
+    duration: 500,
+    type: 'oneByOne',
+    file: "exported-svgs/1.svg"
+  }, func2);
 }
-new Vivus('svg1', {
-  duration: 500,
-  type: 'oneByOne'
-}, myCallback);
+
+function func2() {
+  new Vivus('b3', {
+    duration: 500,
+    type: 'oneByOne',
+    file: "exported-svgs/2.svg"
+  });
+}
